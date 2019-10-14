56ffa565889f33bbe25d72080f5ac202e7a8868c
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2019-10-14 12:51:18 -0300

    Create archivos.sql

---
+++ archivos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE archivos (
+	id int AUTO_INCREMENT NOT NULL,
+	archivo_nombre varchar(255) NOT NULL,
+	archivo_peso varchar(15) NOT NULL,
+	archivo_tipo varchar(25) NOT NULL,
+	archivo_nombre_real varchar(255) NOT NULL,
+	fecha_insercion datetime(3) NULL
+); 
