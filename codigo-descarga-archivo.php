6c7d9a9b400c6c30403026cc27312498662b6dc3
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2019-10-21 15:47:12 -0300

    Create codigo-descarga-archivo.php

---
+++ codigo-descarga-archivo.php
@@ -0,0 +1,21 @@
+//En $direccion_archivo lo cambias por dirección del archivo a descargar ejemplo ./descargar/archivo/texto.txt
+//En $nombre_nuevo_archivo_sin_formato irá el nombre pero ojo debe ir un ".txt" o ".pdf" , o "." cualquier formato para que al descargar no te salga un archivo "descarga" sino "descarga.txt"
+
+$file_example = $direccion_archivo;
+if (file_exists($file_example)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename='.$nombre_nuevo_archivo_sin_formato;
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file_example));
+    ob_clean();
+    flush();
+    readfile($file_example);
+    exit;
+}
+else {
+    echo 'Archivo no disponible.';
+}
